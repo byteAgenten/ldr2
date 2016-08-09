ba822c0822e741d77b3694354c868680a81ab945
matthias <matthias.knoop@byteagenten.de>
2016-08-09 10:33:49 +0200

    ElasticsearchLogWriter initialization implemented Separation lines added to HTML dump from attributes list

--- impl/client/src/main/java/de/byteagenten/ldr2/writer/ElasticsearchLogWriter.java
+++ impl/client/src/main/java/de/byteagenten/ldr2/writer/ElasticsearchLogWriter.java
@@ -24,33 +24,35 @@ public class ElasticsearchLogWriter implements LogWriter {
     public static final String PORT = "ldr2.elasticsearch.port";
 
     @Override
-    public void init(String name, JsonObject writerConfigJsonObject) throws WriterException {
+    public void init(String name, JsonObject configJson) throws WriterException {
 
         this.name = name;
-        /*
-        String hostProperty = properties.getProperty(HOST);
-        if( hostProperty == null ) throw new WriterException(String.format("No host specified. Please add property '%s' to the writer configuration", HOST));
 
-        String portProperty = properties.getProperty(PORT);
-        if( portProperty == null ) throw new WriterException(String.format("No port specified. Please add property '%s' to the writer configuration", PORT));
+        String host = "localhost";
+        Integer port = 9300;
 
-        int port = 0;
-        try {
-            port = Integer.parseInt(portProperty);
-        } catch (NumberFormatException e) {
-            throw new WriterException(String.format("Specified port is not valid [port: %s]", portProperty));
+        if (configJson.has("host")) {
+
+            if (!configJson.get("host").isJsonPrimitive() || !configJson.get("host").getAsJsonPrimitive().isString())
+                throw new WriterException("Specified host is not a string");
+
+            host = configJson.get("host").getAsString();
+        }
+
+        if (configJson.has("port")) {
+
+            if (!configJson.get("port").isJsonPrimitive() || !configJson.get("port").getAsJsonPrimitive().isNumber())
+                throw new WriterException("Specified port is not a number");
+            port = (int) Math.round(configJson.get("port").getAsNumber().doubleValue());
         }
 
         try {
             this.client = TransportClient.builder().build()
-                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(hostProperty), port));
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), port));
         } catch (UnknownHostException e) {
             throw new WriterException("Connecting elasticsearch host failed.", e);
         }
-        */
     }
-
-
 
     @Override
     public void dispose() {
@@ -58,13 +60,12 @@ public class ElasticsearchLogWriter implements LogWriter {
         this.client.close();
     }
 
-
     @Override
     public void write(GenericLogEvent logEvent) {
 
-//        IndexResponse response = client.prepareIndex(logEvent.getApplicationId().toLowerCase(), logEvent.getEventName().toLowerCase())
-//                .setSource(logEvent.getJsonString())
-//                .get();
+        IndexResponse response = client.prepareIndex(logEvent.getApplicationId().toLowerCase(), logEvent.getEventName().toLowerCase())
+                .setSource(logEvent.getJsonString())
+                .get();
     }
 
     @Override
